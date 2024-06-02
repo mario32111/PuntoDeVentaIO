ebd226a7a967f9ab622e1cf89838f35645dafb97
mario garcia estevane <YOUR_EMAIL>
2024-06-02 11:16:51 -0600

    hasta aqui todo bien

--- view/historial.php
+++ view/historial.php
@@ -39,6 +39,8 @@ $result = $conn->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Historial de Ventas</title>
     <link rel="stylesheet" href="../style/app.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;500;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <div class="container">
